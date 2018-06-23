b6b3d65f460e51fb8ca8cf9f8205da4a1a9dc745
Matt McGinnis <mcginnisma90@gmail.com>
2018-06-22 21:32:12 -0400

    Copied .Xmodmap and .Xresources in 300-Configuration-File-Setup.sh.

--- 300-Configuration-File-Setup.sh
+++ 300-Configuration-File-Setup.sh
@@ -6,3 +6,13 @@
 
 # Enable NetworkManager
 sudo systemctle enable NetworkManager.service
+
+#############################
+##### home file changes #####
+#############################
+
+# Add .Xmodmap to ~/
+cp ~/GitHub/Antergos/home/.Xmodmap ~/
+
+# Add .Xresources to ~/
+cp ~/GitHub/Antergos/home/.Xresources ~/
