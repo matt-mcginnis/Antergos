b049c79729a1da3f552f2262ddd0e35e1306fbe1
Matt McGinnis <mcginnisma90@gmail.com>
2018-06-22 20:59:08 -0400

    Created 200-AUR-Package-Installation.sh and installed some basic packages.

---
+++ 200-AUR-Package-Installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##############################################
+########## AUR Package Installation ##########
+##############################################
+
+# Install google chrome
+yaourt google-chrome
+
+# Install hacked-aio-righty cursor
+yaourt hacked-aio-righty
+
+# Install i3exit
+yaourt i3exit
+
+# Install rxvt-unicode-pixbuf
+yaourt rxvt-unicode-pixbuf
+
+# Install urxvt-resize-font-git
+yaourt urxvt-resize-font-git
