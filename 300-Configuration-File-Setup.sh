b9120fc09c8541c56a29e10545ec9e450fd6fcbe
Matt McGinnis <mcginnisma90@gmail.com>
2018-07-01 14:51:10 -0400

    Changed shell to /usr/bin/zsh.

--- 300-Configuration-File-Setup.sh
+++ 300-Configuration-File-Setup.sh
@@ -96,7 +96,7 @@ cp -r ~/GitHub/Antergos/.vim/after/ ~/.vim/
 #####################
 
 # Make zsh the default shell
-chsh -s /bin/zsh
+chsh -s /usr/bin/zsh
 
 # Copy .oh-my-zsh/custom personal configurations
 sudo cp ~/GitHub/Antergos/home/.oh-my-zsh/custom/auto-suggestions.zsh ~/.oh-my-zsh/custom/
